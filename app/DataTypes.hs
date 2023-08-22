a26eb8e5844e03acc9b591f3ef4fe7eddfa29a89
Hukasx0 <hubertkasp13@gmail.com>
2023-08-22 18:57:27 +0200

    Added simple math and logic

--- app/DataTypes.hs
+++ app/DataTypes.hs
@@ -3,23 +3,34 @@ module DataTypes where
 import Text.Parsec
 import Text.Parsec.String
 
-data Value = Str String | Number String | Boolean String | Array [Value]
+data Value = Str String | Number Int | Boolean Bool | Array [Value] | Math Value String Value | Logic Value String Value
 
 
 stringParser :: Parser Value
 stringParser = Str <$> (char '"' >> many1 (noneOf "\"") <* char '"')
 
 numberParser :: Parser Value
-numberParser = Number <$> (many1 digit)
+numberParser = do
+    digits <- many1 digit
+    let intVal = read digits :: Int
+    return (Number intVal)
 
 boolParser :: Parser Value
-boolParser = Boolean <$> ((string "True") <|> (string "False"))
+boolParser = do
+    boolStr <- string "True" <|> string "False"
+    return (Boolean (boolStr == "True"))
 
 valueParser :: Parser Value
 valueParser = try stringParser <|> try numberParser <|> boolParser
 
 arrayParser :: Parser Value
-arrayParser = Array <$> (char '[' >> spaces >> (valueParser `sepBy` (char ',')) <* spaces <* char ']')
+arrayParser = Array <$> (char '[' >> spaces >> (allValsParser `sepBy` (char ',')) <* spaces <* char ']')
+
+mathParser :: Parser Value
+mathParser = Math <$> (numberParser) <*> (spaces >> (try (string "+") <|> try (string "-") <|> try (string "**") <|> try (string "*") <|> try (string "/") <|> string "%")) <*> (spaces >> numberParser)
+
+logicParser :: Parser Value
+logicParser = Logic <$> (boolParser) <*> (spaces >> (string "==" <|> string "!=" <|> string "&&" <|> string "and" <|> string "||" <|> string "or" <|> string "^" <|> string "xor")) <*> (spaces >> boolParser)
 
 allValsParser :: Parser Value
-allValsParser = try stringParser <|> try numberParser <|> try boolParser <|> arrayParser
+allValsParser = try stringParser <|> try mathParser <|> try logicParser <|> try numberParser <|> try boolParser <|> arrayParser
--- app/Interpreter.hs
+++ app/Interpreter.hs
@@ -2,12 +2,41 @@ module Interpreter where
 
 import DataTypes
 import ParseCode
+import Data.Char
 
 executeValue :: Value -> String
 executeValue (Str s) = s
-executeValue (Number n) = n
-executeValue (Boolean b) = b
+executeValue (Number n) = show $ n
+executeValue (Boolean b) = show $ b
 executeValue (Array a) = concat $ map (executeValue) a
+executeValue (Math (Number val1) op (Number val2)) = executeMath val1 op val2
+executeValue (Logic (Boolean val1) op (Boolean val2)) = executeLogic val1 op val2
+executeValue _ = error $ "Incorrect value"
+
+executeMath :: Int -> String -> Int -> String
+executeMath val1 "+" val2 = show $ (val1 + val2)
+executeMath val1 "-" val2 = show $ (val1 - val2)
+executeMath val1 "/" val2 = show $ (val1 `div` val2)
+executeMath val1 "*" val2 = show $ (val1 * val2)
+executeMath val1 "**" val2 = show $ (val1 ^ val2)
+executeMath val1 "%" val2 = show $ (val1 `mod` val2)
+executeMath _ _ _ = ""
+
+xor :: Bool -> Bool -> Bool
+xor True False = True
+xor False True = True
+xor _ _ = False
+
+executeLogic :: Bool -> String -> Bool -> String
+executeLogic val1 "==" val2 = show $ (val1 == val2)
+executeLogic val1 "!=" val2 = show $ (val1 /= val2)
+executeLogic val1 "&&" val2 = show $ (val1 && val2)
+executeLogic val1 "and" val2 = show $ (val1 && val2)
+executeLogic val1 "||" val2 = show $ (val1 || val2)
+executeLogic val1 "or" val2 = show $ (val1 || val2)
+executeLogic val1 "^" val2 = show $ (val1 `xor` val2)
+executeLogic val1 "xor" val2 = show $ (val1 `xor` val2)
+executeLogic _ _ _ = ""
 
 executeCode :: BuiltInFunction -> IO ()
 executeCode (Main code) = mapM_ (executeCode) code 
